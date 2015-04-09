d1ceccb568100b833db704302dde92cf8872937f
amaitland <alex@maitlandcomputing.com.au>
2015-04-09 14:27:56 +1000

    More Sample1ofn renameing

--- Sample1/Sample1/Properties/AssemblyInfo.cs
+++ Sample1/Sample1/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sample1ofn")]
+[assembly: AssemblyTitle("Sample1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sample1ofn")]
+[assembly: AssemblyProduct("Sample1")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
